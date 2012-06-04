fdda3957412d30d492810af4edc679b7927e3e0e
MGZero <steven.calandra@gmail.com>
2012-06-03 23:34:25 -0400

    Added CTileSheet.cs
    Signed-off-by: MGZero <steven.calandra@gmail.com>

--- King of Thieves/King of Thieves/Graphics/CSprite.cs
+++ King of Thieves/King of Thieves/Graphics/CSprite.cs
@@ -10,10 +10,25 @@ namespace King_of_Thieves.Graphics
     class CSprite : CRenderable
     {
 
-        private Texture2D _sprite;
+        private Texture2D _sprite = null;
+        private CTileSheet _sheet = null;
         private Rectangle _size;
 
         public CSprite(Texture2D sprite, Effect shader = null, params VertexPositionColor[] vertices)
+            : base(shader, vertices)
+        {
+            _sprite = sprite;
+            _size = new Rectangle(0, 0, _sprite.Width, _sprite.Height);
+        }
+
+        public CSprite(CTileSheet spriteSheet, Effect shader = null, params VertexPositionColor[] vertices)
+            :base(shader, vertices)
+        {
+            _sheet = spriteSheet;
+            _size = new Rectangle(0, 0, _sprite.Width, _sprite.Height);
+        }
+
+        public CSprite(Texture2D sprite, Rectangle size, Effect shader = null, params VertexPositionColor[] vertices)
             : base(shader, vertices)
         {
             _sprite = sprite;
--- King of Thieves/King of Thieves/Input/CInput.cs
+++ King of Thieves/King of Thieves/Input/CInput.cs
@@ -9,9 +9,9 @@ namespace King_of_Thieves.Input
 {
     static class CInput
     {
-<<<<<<< HEAD
+//<<<<<<< HEAD
         //git sanity test, don't mind me. -Steve
-=======
+//=======
         private static GamePadState _padStateCurrent;
         private static KeyboardState _keyStateCurrent;
         private static MouseState _mouseStateCurrent;
@@ -21,7 +21,7 @@ namespace King_of_Thieves.Input
         private static MouseState _mouseStatePrevious;
 
         //sanity test for git, don't mind me. -Steve
->>>>>>> master
+//>>>>>>> master
         public static bool getInputDown(Buttons button)
         {
             if (!_padStateCurrent.IsConnected)
