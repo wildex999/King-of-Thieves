3c3e8d6382c69d694cf3ac23c447888b253a8f76
MGZero <steven.calandra@gmail.com>
2012-06-04 21:15:03 -0400

    Doing some...things....
    Signed-off-by: MGZero <steven.calandra@gmail.com>

---
+++ King of Thieves/King of Thieves/CMasterControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace King_of_Thieves
+{
+    static class CMasterControl
+    {
+        public static Queue<Graphics.CRenderable> drawQueue = new Queue<Graphics.CRenderable>();
+
+       
+    }
+}
