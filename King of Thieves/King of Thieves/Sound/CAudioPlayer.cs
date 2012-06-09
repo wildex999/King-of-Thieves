4976620509fe13434fc1b8d938ad64a4b4ed1f41
MGZero <steven.calandra@gmail.com>
2012-06-09 18:18:13 -0400

    Sound stuff on its way!
    Signed-off-by: MGZero <steven.calandra@gmail.com>

---
+++ King of Thieves/King of Thieves/Sound/CAudioPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace King_of_Thieves.Sound
+{
+    class CAudioPlayer
+    {
+        private Thread _audioThread;
+
+        public void playSong()
+        {
+            
+        }
+    }
+}
---
+++ King of Thieves/King of Thieves/Sound/CSound.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+
+namespace King_of_Thieves.Sound
+{
+    class CSound
+    {
+        private SoundEffect _sfx;
+
+        public CSound(SoundEffect fx)
+        {
+            _sfx = fx;
+           // MediaPlayer.Play(
+        }
+    }
+}
