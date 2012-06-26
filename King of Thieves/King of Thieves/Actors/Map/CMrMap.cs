45cb584887b6bb157bb0f87bc99a2a08ec256dd8
gm112 <gm112@zfgc.com>
2012-06-26 00:36:54 -0400

    Skeletons for the big, big MrMap. :3
    Signed-off-by: gm112 <gm112@zfgc.com>

---
+++ King of Thieves/King of Thieves/Actors/Map/CMrMap.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace King_of_Thieves.Actors.Map
+{
+    class CMrMap : CActor
+    {
+        public CMrMap()
+            : base("MrMap", ACTORTYPES.MANAGER)
+        {
+
+        }
+        public override void create(object sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void destroy(object sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void keyDown(object sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void keyRelease(object sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void frame(object sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void draw(object sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void collide(object sender, object collider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void animationEnd(object sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void _addCollidables()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ King of Thieves/King of Thieves/Input/CXMLSerializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace King_of_Thieves.Input
+{
+    static class CXMLSerializer
+    {
+        public CXMLSerializer()
+        {
+
+        }
+
+        public void Seek()
+        {
+
+        }
+
+        public void Write()
+        {
+
+        }
+    }
+}
