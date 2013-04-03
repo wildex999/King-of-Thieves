37e42a36a182fdccc81ac49460d3ffc533a3aad0
MGZero <steven.calandra@gmail.com>
2013-04-02 23:06:50 -0400

    started npcs
    Signed-off-by: MGZero <steven.calandra@gmail.com>

---
+++ King of Thieves/King of Thieves/Actors/NPC/Enemies/CBaseEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using King_of_Thieves.Graphics;
+using Gears.Cloud;
+using King_of_Thieves.Input;
+namespace King_of_Thieves.Actors.NPC.Enemies
+{
+    //has all the things that an enemy NPC will have
+    //ex: item drops
+    abstract class CBaseEnemy : CActor
+    {
+        protected Dictionary<object,float> _itemDrop; //leave this as object until we have classes for items ready
+        
+
+        protected abstract void _addCollidables();
+
+        public override void destroy(object sender)
+        {
+            _dropItem();
+
+            base.destroy(sender);
+        }
+
+        private void _dropItem()
+        {
+            object itemToDrop = null;
+
+
+        }
+    }
+}
---
+++ King of Thieves/King of Thieves/Actors/NPC/Enemies/Chuchus/CBaseChuChu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using King_of_Thieves.Graphics;
+using Gears.Cloud;
+using King_of_Thieves.Input;
+
+namespace King_of_Thieves.Actors.NPC.Enemies.Chuchus
+{
+    //this class should implement everything that's consistent across all chu chus
+    abstract class CBaseChuChu : CBaseEnemy
+    {
+
+    }
+}
